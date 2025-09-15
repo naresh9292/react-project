294f1668cf1edb5633f82f04ffdc1bac35d31701
naresh9292 <90213679+naresh9292@users.noreply.github.com>
2025-09-15 18:00:21 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Home, {Login, Contactus, Aboutus} from './Home';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Login />
+      <Aboutus />
+      <Contactus />
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+    return <h2>Home</h2>;
+}
+
+export function Login() {
+    return <h2>Login</h2>;
+}
+
+export function Contactus() {
+    return <h2>Contactus</h2>;
+}
+
+export function Aboutus() {
+    return <h2>Aboutus</h2>;
+}
+
+export default Home
